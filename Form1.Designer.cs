bf29657ea0f00f5214718b510f232d40d76235c7
Dhanapaal <33400701+Dhanapaal@users.noreply.github.com>
2017-11-05 19:03:31 +0000

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnMessage = new System.Windows.Forms.Button();
+            this.btnStation = new System.Windows.Forms.Button();
+            this.btnAnnouncement = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnStartStop = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.txtInterval = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnMessage
+            // 
+            this.btnMessage.Location = new System.Drawing.Point(6, 19);
+            this.btnMessage.Name = "btnMessage";
+            this.btnMessage.Size = new System.Drawing.Size(235, 27);
+            this.btnMessage.TabIndex = 0;
+            this.btnMessage.Text = "Message";
+            this.btnMessage.UseVisualStyleBackColor = true;
+            this.btnMessage.Click += new System.EventHandler(this.btnMessage_Click);
+            // 
+            // btnStation
+            // 
+            this.btnStation.Location = new System.Drawing.Point(6, 58);
+            this.btnStation.Name = "btnStation";
+            this.btnStation.Size = new System.Drawing.Size(235, 27);
+            this.btnStation.TabIndex = 1;
+            this.btnStation.Text = "Station";
+            this.btnStation.UseVisualStyleBackColor = true;
+            this.btnStation.Click += new System.EventHandler(this.btnStation_Click);
+            // 
+            // btnAnnouncement
+            // 
+            this.btnAnnouncement.Location = new System.Drawing.Point(6, 94);
+            this.btnAnnouncement.Name = "btnAnnouncement";
+            this.btnAnnouncement.Size = new System.Drawing.Size(235, 27);
+            this.btnAnnouncement.TabIndex = 2;
+            this.btnAnnouncement.Text = "Announcement";
+            this.btnAnnouncement.UseVisualStyleBackColor = true;
+            this.btnAnnouncement.Click += new System.EventHandler(this.btnAnnouncement_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Red;
+            this.btnExit.Location = new System.Drawing.Point(22, 232);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(235, 27);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 30000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnMessage);
+            this.groupBox1.Controls.Add(this.btnStation);
+            this.groupBox1.Controls.Add(this.btnAnnouncement);
+            this.groupBox1.Location = new System.Drawing.Point(16, 90);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(257, 136);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Adhoc updates";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.btnStartStop);
+            this.groupBox2.Controls.Add(this.btnUpdate);
+            this.groupBox2.Controls.Add(this.txtInterval);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(16, 3);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(257, 81);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Auto Update";
+            // 
+            // btnStartStop
+            // 
+            this.btnStartStop.Location = new System.Drawing.Point(92, 19);
+            this.btnStartStop.Name = "btnStartStop";
+            this.btnStartStop.Size = new System.Drawing.Size(68, 19);
+            this.btnStartStop.TabIndex = 9;
+            this.btnStartStop.Text = "START";
+            this.btnStartStop.UseVisualStyleBackColor = true;
+            this.btnStartStop.Click += new System.EventHandler(this.btnStartStop_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(183, 52);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(68, 19);
+            this.btnUpdate.TabIndex = 8;
+            this.btnUpdate.Text = "UPDATE";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // txtInterval
+            // 
+            this.txtInterval.Location = new System.Drawing.Point(123, 52);
+            this.txtInterval.Name = "txtInterval";
+            this.txtInterval.Size = new System.Drawing.Size(55, 20);
+            this.txtInterval.TabIndex = 7;
+            this.txtInterval.Text = "30";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Change refresh interval";
+            // 
+            // label1
+            // 
+            this.label1.AutoEllipsis = true;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(228, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(285, 271);
+            this.ControlBox = false;
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnExit);
+            this.Name = "Form1";
+            this.Text = "Train Info";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMessage;
+        private System.Windows.Forms.Button btnStation;
+        private System.Windows.Forms.Button btnAnnouncement;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnStartStop;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.TextBox txtInterval;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;  
+using System.Xml;
+using System.Xml.Linq;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            (new ToolTip()).SetToolTip(btnMessage, "Train traffic announcement, such as information about track work, train failures, malfunctions and the like.");
+            (new ToolTip()).SetToolTip(btnStation, "Trafficking, both with and without travel.");
+            (new ToolTip()).SetToolTip(btnAnnouncement, "Timetable information, ie information on trains at traffic points (stations, stops) each mail corresponds to a particular train at the respective traffic location.");
+
+        }
+
+        private void btnMessage_Click(object sender, EventArgs e)
+        {
+            InvokeService("TrainMessage");
+        }
+
+        private void btnStation_Click(object sender, EventArgs e)
+        {
+            InvokeService("TrainStation");
+
+        }
+
+        private void btnAnnouncement_Click(object sender, EventArgs e)
+        {
+          //  InvokeService("TrainAnnouncement");
+
+        }
+
+        void InvokeService(string strCallType)
+        {
+
+            WebClient webclient = new WebClient();
+            webclient.Headers.Add("Referer", "http://www.example.com"); // Replace with your domain here
+            // Registrer a handler that will execute when download is completed.
+            webclient.UploadStringCompleted += (obj, arguments) =>
+            {
+                if (arguments.Cancelled == true)
+                {
+                    MessageBox.Show("Request cancelled by user");
+                }
+                else if (arguments.Error != null)
+                {
+                    MessageBox.Show("Request Failed : " + arguments.Error.Message);
+                }
+                else
+                {
+                    UpdateMessagesDataToDB(strCallType, formatXML(arguments.Result));
+                }
+            };
+
+            try
+            {
+                // API server url
+                Uri address = new Uri("http://api.trafikinfo.trafikverket.se/v1.3/data.xml");
+                string requestBody = "<REQUEST>" +
+                                        // Use your valid authenticationkey
+                                        "<LOGIN authenticationkey='827a4f239b5040d8969c2b5d5e6f733a'/>" +
+                                        "<QUERY objecttype='" + strCallType + "' >" +
+                                            "<FILTER/>" +
+                                            "<EXCLUDE>Deleted</EXCLUDE>" +
+                                        "</QUERY>" +
+                                    "</REQUEST>";
+
+                webclient.Headers["Content-Type"] = "text/xml";
+                webclient.Encoding = System.Text.Encoding.UTF8;
+                webclient.UploadStringAsync(address, "POST", requestBody);
+            }
+
+            catch (UriFormatException)
+            {
+                MessageBox.Show("Malformed url, press 'X' to exit.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occured, press 'X' to exit. :" + ex.Message);
+            }
+        }
+
+        string GetMultipleValues(XmlNode rootNode,string strKey)
+        {
+            string strReturn = "";
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(rootNode.OuterXml);
+            XmlNodeList parentNode = xmlDoc.GetElementsByTagName(strKey);
+            foreach (XmlNode childrenNode in parentNode)
+            {
+                strReturn = strReturn + childrenNode.InnerText + ";";
+            }
+                return strReturn;
+        }
+
+        void UpdateMessagesDataToDB(string strCallType, string xmlResponse)
+        {
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(xmlResponse);
+            MySqlConnection conn = new MySqlConnection("Server=localhost;Database=trafikinfo_schema;Uid=root;Pwd=India@123");
+            conn.Open();
+            {
+                string sqlTruncate = "";
+                switch (strCallType)
+                {
+                    case "TrainMessage":
+                        sqlTruncate = "DELETE FROM TrainMessages";
+                        break;
+                    case "TrainStation":
+                        sqlTruncate = "DELETE FROM TrainStations";
+                        break;
+                    case "TrainAnnouncement":
+                        sqlTruncate = "DELETE FROM TrainAnnouncements";
+                        break;
+                }
+                MySqlCommand commTruncate = conn.CreateCommand();
+                commTruncate.CommandText = sqlTruncate;
+                commTruncate.ExecuteNonQuery();
+
+                XmlNodeList parentNode = xmlDoc.GetElementsByTagName(strCallType);
+                foreach (XmlNode childrenNode in parentNode)
+                {
+                    MySqlCommand comm;
+                    string sql="";
+                    switch (strCallType)
+                    {
+                        case "TrainMessage":
+                            sql = "INSERT INTO TrainMessages(AffectedLocations,    CountyNo,    ExternalDescription,    Geometry,    EventId,    ReasonCodeText,    StartDateTime,    LastUpdateDateTime,    ModifiedTime ) VALUES (";
+                            sql = sql + "'" + GetMultipleValues(childrenNode,"AffectedLocation") + "',";
+                            sql = sql + childrenNode.SelectSingleNode("CountyNo").InnerText + ",";
+                            sql = sql + "'" + childrenNode.SelectSingleNode("ExternalDescription").InnerText + "',";
+                            sql = sql + "'" + childrenNode.SelectSingleNode("Geometry").InnerText + "',";
+                            sql = sql + "'" + childrenNode.SelectSingleNode("EventId").InnerText + "',";
+                            sql = sql + "'" + childrenNode.SelectSingleNode("ReasonCodeText").InnerText + "',";
+                            sql = sql + "'" + System.DateTime.Parse(childrenNode.SelectSingleNode("StartDateTime").InnerText).ToString("yyyy-MM-dd HH:mm") + "',";
+                            sql = sql + "'" + System.DateTime.Parse(childrenNode.SelectSingleNode("LastUpdateDateTime").InnerText).ToString("yyyy-MM-dd HH:mm") + "',";
+                            sql = sql + "'" + System.DateTime.Parse(childrenNode.SelectSingleNode("ModifiedTime").InnerText).ToString("yyyy-MM-dd HH:mm") + "')";
+                            break;
+                       case "TrainStation":
+                            sql = "INSERT INTO TrainStations(Advertised,    AdvertisedLocationName,    AdvertisedShortLocationName,    CountryCode,    CountyNo,    LocationInformationText,    LocationSignature,    ModifiedTime ,PlatformLines,Geometry,Prognosticated) VALUES (";
+                            sql = sql + "'" + childrenNode.SelectSingleNode("Advertised").InnerText + "',";
+                            sql = sql + "'" + childrenNode.SelectSingleNode("AdvertisedLocationName").InnerText + "',";
+                            sql = sql + "'" + childrenNode.SelectSingleNode("AdvertisedShortLocationName").InnerText + "',";
+                            sql = sql + "'" + childrenNode.SelectSingleNode("CountryCode").InnerText + "',";
+                            //Some response dont have CountyNo
+                            try
+                            {
+                                sql = sql + childrenNode.SelectSingleNode("CountyNo").InnerText + ",";
+                            }
+                            catch
+                            {
+                                sql = sql + "null,";
+                            }
+                            sql = sql + "'',";
+                            //sql = sql + "'" + childrenNode.SelectSingleNode("LocationInformationText").InnerText + "',";
+                            sql = sql + "'" + childrenNode.SelectSingleNode("LocationSignature").InnerText + "',";
+                            sql = sql + "'" + System.DateTime.Parse(childrenNode.SelectSingleNode("ModifiedTime").InnerText).ToString("yyyy-MM-dd HH:mm") + "',";
+                            sql = sql + "'" + GetMultipleValues(childrenNode, "PlatformLine") + "',";
+                            sql = sql + "'" + childrenNode.SelectSingleNode("Geometry").InnerText + "',";
+                            sql = sql + "'" + childrenNode.SelectSingleNode("Prognosticated").InnerText + "')";
+                            break;
+                        case "TrainAnnouncement":
+                            break;
+                    }
+                    comm = conn.CreateCommand();
+                    comm.CommandText = sql;
+                    comm.ExecuteNonQuery();
+                }
+            }
+            conn.Close();
+
+        }
+
+        // Format xml so it is readable by humans.
+        private static string formatXML(string xml)
+        {
+            // Format xml.
+            XDocument rxml = XDocument.Parse(xml);
+            XmlWriterSettings xmlsettings = new XmlWriterSettings();
+            xmlsettings.OmitXmlDeclaration = true;
+            xmlsettings.Indent = true;
+            xmlsettings.IndentChars = "      ";
+            var sb = new StringBuilder();
+            using (XmlWriter xmlWriter = XmlWriter.Create(sb, xmlsettings))
+            {
+                rxml.WriteTo(xmlWriter);
+            }
+            return sb.ToString();
+        }
+         
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnStartStop_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                timer1.Enabled = false;
+                btnStartStop.Text = "START";
+
+            }
+            else
+            {
+                timer1.Enabled = true;
+                btnStartStop.Text = "STOP";
+
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            timer1.Interval = Int32.Parse(txtInterval.Text) *1000;
+            btnStartStop.Text = "START";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            InvokeService("TrainMessage");
+            System.Threading.Thread.Sleep(30000);
+            InvokeService("TrainStation");
+            System.Threading.Thread.Sleep(30000);
+          //  InvokeService("TrainAnnouncement");
+          //  System.Threading.Thread.Sleep(10000);
+            timer1.Enabled = true;
+            label1.Text = (Int32.Parse(label1.Text) + 1).ToString();
+        }
+    }
+
+}
+
+
